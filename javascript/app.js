01607cf85b5e2bd8e031d0ae0d697e6d64ef069b
michaelgrn <michaeljamesgreen86@gmail.com>
2022-09-24 02:38:39 -0600

    MVP with statically generated HTML for each day of the week. There exists the possibility to have JS dynamically generate this HMTL. However, the first step is always to get the software working, and then make it good.

--- javascript/app.js
+++ javascript/app.js
@@ -21,19 +21,21 @@ let weather = {
     showWeather: function(data){
         const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
         const {city_name} = data;
-        const {datetime, high_temp, low_temp, wind_spd, wind_cdir} = data['data']['0'];
-        const {icon, description} = data['data']['0']["weather"];
-        const d = new Date(datetime)
-        let day = weekday[d.getDay()];
-        console.log(city_name, day, datetime, low_temp, high_temp, description,icon, wind_spd, wind_cdir);
-        document.querySelector(".day").innerText = day;
-        document.querySelector(".date").innerText = datetime;
-        document.querySelector(".temp-low").innerText = "Low: " + low_temp;
-        document.querySelector(".temp-high").innerText = "High: " + high_temp;
-        document.querySelector(".weather-state").innerHTML = description;
-        document.querySelector(".icon").src = "https://www.weatherbit.io/static/img/icons/"+ icon+".png";
-        document.querySelector(".wind-speed").innerHTML = wind_spd + "mph";
-        document.querySelector(".wind-direction").innerHTML = wind_cdir;
+        for(let x = 0; x < 7; x++) {
+            const {datetime, high_temp, low_temp, wind_spd, wind_cdir} = data['data'][x.toString()];
+            const {icon, description} = data['data'][x.toString()]["weather"];
+            const d = new Date(datetime)
+            let day = weekday[d.getDay()];
+            console.log(city_name, day, datetime, low_temp, high_temp, description, icon, wind_spd, wind_cdir);
+            document.querySelector("#day-"+x.toString()).innerText = day;
+            document.querySelector("#date-"+x.toString()).innerText = datetime;
+            document.querySelector("#temp-low-"+x.toString()).innerText = "Low: " + low_temp;
+            document.querySelector("#temp-high-"+x.toString()).innerText = "High: " + high_temp;
+            document.querySelector("#weather-state-"+x.toString()).innerHTML = description;
+            document.querySelector("#icon-"+x.toString()).src = "https://www.weatherbit.io/static/img/icons/" + icon + ".png";
+            document.querySelector("#wind-speed-"+x.toString()).innerHTML = wind_spd + "mph";
+            document.querySelector("#wind-direction-"+x.toString()).innerHTML = wind_cdir;
+        }
     },
     search: function () {
         this.getWeather(document.querySelector(".search-bar").value);
