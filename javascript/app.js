f18f7144195eec8580a692041ce6bf53b5bf8699
michaelgrn <michaeljamesgreen86@gmail.com>
2022-09-24 22:34:03 -0600

    Primary focus of this commit is implementing CSS changes. On page load will automatically display weather for Boise. Still need to write README for npm implementation on other devices, test on mobile, handle errors, and complete documentation.

--- javascript/app.js
+++ javascript/app.js
@@ -19,18 +19,17 @@ let weather = {
     // ● Weather Icons (matching weather state)
     // ● Wind speed & direction
     showWeather: function(data){
-        const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        const weekday = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
         const {city_name} = data;
         for(let x = 0; x < 7; x++) {
             const {datetime, high_temp, low_temp, wind_spd, wind_cdir} = data['data'][x.toString()];
             const {icon, description} = data['data'][x.toString()]["weather"];
             const d = new Date(datetime)
             let day = weekday[d.getDay()];
+            let date = datetime.split("-")[2];
             console.log(city_name, day, datetime, low_temp, high_temp, description, icon, wind_spd, wind_cdir);
-            document.querySelector("#day-"+x.toString()).innerText = day;
-            document.querySelector("#date-"+x.toString()).innerText = datetime;
-            document.querySelector("#temp-low-"+x.toString()).innerText = "Low: " + low_temp;
-            document.querySelector("#temp-high-"+x.toString()).innerText = "High: " + high_temp;
+            document.querySelector("#date-"+x.toString()).innerHTML = "<b>" + day + "</b>/" + date;
+            document.querySelector("#temp-"+x.toString()).innerHTML = "<b>" + high_temp + "°" + "</b>/" + low_temp + "°";
             document.querySelector("#weather-state-"+x.toString()).innerHTML = description;
             document.querySelector("#icon-"+x.toString()).src = "https://www.weatherbit.io/static/img/icons/" + icon + ".png";
             document.querySelector("#wind-speed-"+x.toString()).innerHTML = wind_spd + "mph";
@@ -42,7 +41,7 @@ let weather = {
     },
 };
 
-document.querySelector(".search button").addEventListener("click", function () {
+document.querySelector(".search-button").addEventListener("click", function () {
     weather.search();
 });
 
@@ -53,3 +52,5 @@ document
             weather.search();
         }
     });
+
+weather.getWeather("Boise,ID");
