5be9eebeffd097f8e6ad51f0de66b8847efee4d9
michaelgrn <michaeljamesgreen86@gmail.com>
2022-09-28 02:01:37 -0600

    Improved Mobile UI (MVP?). Added selection of location from drop down menu for search. Still need to focus on the 4 other breakpoints. Python code is used for extracting cities from csv.

--- index.js
+++ index.js
@@ -41,6 +41,11 @@ const server = http.createServer((req, res) => {
             res.setHeader('Content-Type', 'image/png');
             fs.createReadStream(filePath).pipe(res);
         }
+        else if (fileExt == '.txt') {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            fs.createReadStream(filePath).pipe(res);
+        }
         else {
             filePath = path.resolve('./public/404.html');
             res.statusCode = 404;
