cc7ab5e8ebdad2b5670e1dc7b4e1f0f9c0c45629
michaelgrn <michaeljamesgreen86@gmail.com>
2022-10-12 01:20:50 -0600

    Updated node.js to be more streamlined and moved weather and autocomplete to seperate backend files. Still need to make .js files private.

---
+++ public/js/autocomplete.js
@@ -0,0 +1,147 @@
+/**
+ * The purpose of this function is to open the preprocessed cities.txt
+ * and populate the cities variable for autocomplete to leverage. Uses
+ * XMLHttpRequest due to problems with Fetch().
+ * @param  {string} filePath relative path to cities.txt
+ * @return {listOfCities} list of cities
+ */
+function loadFile(filePath) {
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", filePath, false);
+    xmlhttp.send();
+    if (xmlhttp.status==200) {
+        listOfCities = xmlhttp.responseText;
+    }
+    console.log(typeof result);
+    return listOfCities;
+}
+
+/**
+ * Function used to generate the auto-complete field in the search bar.
+ * Taken from https://www.w3schools.com/howto/howto_js_autocomplete.asp
+ *
+ * @param  {string} inp text input in search bar
+ * @param  {string} arr array of words to eligible for auto-complete
+ */
+function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        let numCities = 0 // Limits number of cities appearing in the autocomplete
+        /*for each item in the array...*/
+        for (i = 0; i < arr.length; i++) {
+            /*check if the item starts with the same letters as the text field value:*/
+
+            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase() && numCities < 10) { /*here to limit options?*/
+                /*create a DIV element for each matching element:*/
+                b = document.createElement("DIV");
+                /*make the matching letters bold:*/
+                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                b.innerHTML += arr[i].substr(val.length);
+                /*insert a input field that will hold the current array item's value:*/
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function(e) {
+                    /*insert the value for the autocomplete text field:*/
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    /*close the list of autocompleted values,
+                    (or any other open lists of autocompleted values:*/
+                    closeAllLists();
+                    weather.search();
+
+                });
+                a.appendChild(b);
+                numCities = numCities+1;
+                console.log(numCities);
+            }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+            /*If the arrow DOWN key is pressed,
+            increase the currentFocus variable:*/
+            currentFocus++;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 38) { //up
+            /*If the arrow UP key is pressed,
+            decrease the currentFocus variable:*/
+            currentFocus--;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            /*If the ENTER key is pressed, prevent the form from being submitted,*/
+            e.preventDefault();
+            if (currentFocus > -1) {
+                /*and simulate a click on the "active" item:*/
+                if (x) x[currentFocus].click();
+            }
+        }
+    });
+    function addActive(x) {
+        /*a function to classify an item as "active":*/
+        if (!x) return false;
+        /*start by removing the "active" class on all items:*/
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        /*add class "autocomplete-active":*/
+        x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+        /*a function to remove the "active" class from all autocomplete items:*/
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+    function closeAllLists(elmnt) {
+        /*close all autocomplete lists in the document,
+        except the one passed as an argument:*/
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+
+    document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            closeAllLists();
+        }
+    });
+}
+
+
+
+
+
+// Load list of cities into cities variable for autocomplete()
+//let cities = loadFile("./data/cities.txt").split("\n")
+// let cities = loadFile("./data/cities1K.txt").split("\n")
+let cities = loadFile("../data/cities15K.txt").split("\n")
+
+
+// Activate autocomplete()
+autocomplete(document.getElementById("weather-search"), cities);
---
+++ public/js/weather.js
@@ -0,0 +1,131 @@
+/**
+ * Object designed to access weatherbit.io and populate the webapp
+ * with weather information.
+ */
+let weather = {
+
+    "apikey": "7374afdfe8c04b72931d05680d8f1717",
+
+    /**
+     * Function designed to fetch api information and pass that json
+     * off to showWeather()
+     *
+     * @param  {string} city Name of the city to generate the forecast for
+     */
+    getWeather: function(city){
+        fetch(
+            "https://api.weatherbit.io/v2.0/forecast/daily?city="
+            + city
+            +"&units=I"
+            +"&days=7"
+            +"&key="
+            + this.apikey
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    alert("No weather found. Please double check your that your query matches the format of \"City, State, Country\" or \"City, County\".");
+                    throw new Error("No weather found.");
+                }
+                return response.json();
+            })
+            .then((data) => this.showWeather(data));
+    },
+    /**
+     * Function designed to populate the webpage with the following information
+     *      ● Day of the week
+     *      ● Date
+     *      ● Temperature (high and low)
+     *      ● Weather State (sunny, cloudly, rainy, snowy, etc)
+     *      ● Weather Icons (matching weather state)
+     *      ● Wind speed & direction
+     * Code currently commented out to populate name of city in designated field
+     * for later iteration.
+     * @param  {json} data  json from weatherbit.io api call
+     */
+    showWeather: function(data){
+
+        const weekday = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
+        const {city_name, state_code, country_code} = data;
+
+        for(let x = 0; x < 7; x++) {
+            const {datetime, high_temp, low_temp, wind_spd, wind_cdir} = data['data'][x.toString()];
+            const {icon, description} = data['data'][x.toString()]["weather"];
+            const d = new Date(datetime)
+
+            let day = weekday[d.getDay()];
+            let date = datetime.split("-")[2];
+
+            document.querySelector("#date-"+x.toString()).innerHTML = day + " " + date;
+            document.querySelector("#temp-"+x.toString()).innerHTML = "<b>" + high_temp + "°" + "</b>/" + low_temp + "°";
+            document.querySelector("#weather-state-"+x.toString()).innerHTML = description;
+            document.querySelector("#icon-"+x.toString()).src = "img/icons/" + icon + ".png";
+            document.querySelector("#wind-speed-"+x.toString()).innerHTML ="<img src = img/icons/icon-wind.png alt=\"Small icon depicting the wind\" id = \"wind-speed-icon\"> " + wind_cdir + " " + wind_spd + " mph " ;
+        }
+
+        // clears the Search-Bar
+        document.querySelector(".city-current").innerHTML =  city_name;
+        if (state_code.match(/\d+/g) != null) {
+            document.querySelector(".city-current-full").innerHTML =  city_name + ", "  + country_code;
+        } else {
+            document.querySelector(".city-current-full").innerHTML =  city_name +", "+ state_code + ", " + country_code;
+        }
+        document.getElementById('weather-search').value = '';
+        weather.getCurrent(city_name)
+
+    },
+    /**
+     * Function designed to get current Temperature of designated city.
+     * @param {string} city Name of city to get current forecast for
+     */
+    getCurrent: function(city){
+        fetch(
+            "https://api.weatherbit.io/v2.0/current?city="
+            + city
+            +"&units=I"
+            +"&key="
+            + this.apikey
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    alert("No weather found. Please double check your that your query matches the format of \"City, State, Country\" or \"City, County\".");
+                    throw new Error("No weather found.");
+                }
+                return response.json();
+            })
+            .then((data) => this.showCurrent(data));
+
+    },
+    /**
+     * Function used in conjunction with getCurrent() to populate fields         *
+     * @param {json} data Data for the city to get current forecast for
+     */
+    showCurrent: function(data){
+        console.log(data['data']['0']);
+        const {app_temp} = data['data']['0'];
+        const {icon} = data['data']['0']['weather'];
+        document.querySelector("#icon-current").src = "img/icons/" + icon + ".png";
+        document.querySelector(".temp-current").innerHTML = app_temp + "°F";
+
+    },
+    /**
+     * Function used with a listener to pass value of search-bar to getWeather
+     */
+    search: function () {
+        this.getWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+// Add listener to search-button
+document.querySelector(".search-button").addEventListener("click", function () {
+    weather.search();
+});
+
+// Add listener to search-bar
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+});
+
+// Autopopulate website with Boise weather
+weather.getWeather("Boise,ID");
